cc031f6f37507da8818fdd235cd0cc83ebb0d343
Dominic Canare <dom@dominiccanare.com>
2025-05-14 08:39:58 -0500

    Cancellable jobs

--- src/pupil_labs/neon_player/console.py
+++ src/pupil_labs/neon_player/console.py
@@ -8,6 +8,7 @@ from PySide6.QtWidgets import (
     QPushButton,
     QSizePolicy,
     QTextEdit,
+    QToolButton,
     QVBoxLayout,
     QWidget,
 )
@@ -17,12 +18,27 @@ from pupil_labs.neon_player.expander import Expander
 from pupil_labs.neon_player.job_manager import BGWorker
 
 
-class JobProgressBar(QProgressBar):
+class JobProgressBar(QWidget):
     def __init__(self, worker: BGWorker, *args: T.Any, **kwargs: T.Any) -> None:
         super().__init__(*args, **kwargs)
 
+        self.main_layout = QHBoxLayout()
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.main_layout)
+
+        self.progress_bar = QProgressBar()
+        self.main_layout.addWidget(self.progress_bar)
+
+        self.cancel_button = QToolButton()
+        self.cancel_button.setText("🗑")
+        self.cancel_button.setAutoRaise(True)
+        self.cancel_button.clicked.connect(worker.cancel)
+        self.main_layout.addWidget(self.cancel_button)
+
         self.worker = worker
-        self.worker.qt_helper.progress_changed.connect(lambda v: self.setValue(v * 100))
+        self.worker.qt_helper.progress_changed.connect(
+            lambda v: self.progress_bar.setValue(v * 100)
+        )
 
 
 class ConsoleWindow(QWidget):
@@ -38,6 +54,7 @@ class ConsoleWindow(QWidget):
         # Job list section
         self.job_table = QWidget()
         self.job_table_layout = QFormLayout()
+        self.job_table_layout.setSpacing(3)
         self.job_table.setLayout(self.job_table_layout)
 
         self.job_table_expander = Expander(title="Jobs")
@@ -47,15 +64,21 @@ class ConsoleWindow(QWidget):
         # Log section
         self.console_widget = QTextEdit()
         self.console_widget.setReadOnly(True)
-        self.console_widget.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        self.console_widget.setTextInteractionFlags(
+            Qt.TextInteractionFlag.TextSelectableByMouse
+        )
 
         self.console_expander = Expander(title="Console")
         self.console_expander.set_content_widget(self.console_widget)
-        self.console_expander.expanded_changed.connect(lambda _: self.update_stretches())
+        self.console_expander.expanded_changed.connect(
+            lambda _: self.update_stretches()
+        )
         self.main_layout.addWidget(self.console_expander)
 
         self.spacer = QWidget()
-        self.spacer.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.MinimumExpanding)
+        self.spacer.setSizePolicy(
+            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.MinimumExpanding
+        )
         self.main_layout.addWidget(self.spacer)
 
         # Buttons
@@ -69,13 +92,14 @@ class ConsoleWindow(QWidget):
 
         app = neon_player.instance()
         app.job_manager.job_started.connect(self.on_job_added)
-        app.job_manager.job_finished.connect(self.on_job_finished)
+        app.job_manager.job_finished.connect(self.remove_job)
+        app.job_manager.job_canceled.connect(self.remove_job)
         app.job_manager.progress_changed.connect(self.on_total_updated)
 
     def on_job_added(self, worker: BGWorker) -> None:
         self.job_table_layout.addRow(worker.name, JobProgressBar(worker))
 
-    def on_job_finished(self, worker: BGWorker) -> None:
+    def remove_job(self, worker: BGWorker) -> None:
         for row_idx in range(self.job_table_layout.rowCount()):
             item = self.job_table_layout.itemAt(row_idx, QFormLayout.ItemRole.FieldRole)
             widget = item.widget()
--- src/pupil_labs/neon_player/job_manager.py
+++ src/pupil_labs/neon_player/job_manager.py
@@ -1,4 +1,3 @@
-import contextlib
 import multiprocessing as mp
 import multiprocessing.connection
 import signal
@@ -8,11 +7,17 @@ import typing as T
 
 from PySide6.QtCore import QObject, QTimer, Signal
 
+from pupil_labs import neon_player
+
 
 class ProgressUpdate:
-    def __init__(self, progress: float, datum : T.Any = None) -> None:
+    def __init__(self, progress: float, datum: T.Any = None) -> None:
         self.progress = progress
         self.datum = datum
+
+
+class EarlyCancellationError(Exception):
+    pass
 
 
 class BGWorkerQtHelper(QObject):
@@ -39,20 +44,21 @@ class BGWorkerQtHelper(QObject):
 class BGWorker:
     """Future-like object. Iterates a generator in the background"""
 
-    def __init__(self, name: str, generator: T.Callable, *args: T.Any, **kwargs: T.Any) -> None:
+    def __init__(
+        self, name: str, generator: T.Callable, *args: T.Any, **kwargs: T.Any
+    ) -> None:
         super().__init__()
         self.qt_helper = BGWorkerQtHelper(self)
         self.name = name
 
-        ctx = mp.get_context('spawn')
+        ctx = mp.get_context("spawn")
 
         self._completed = False
         self._canceled = False
+        self._cancel_event = ctx.Event()
 
         pipe_recv, pipe_send = ctx.Pipe(False)
-        wrapper_args = [
-            pipe_send, generator
-        ]
+        wrapper_args = [pipe_send, self._cancel_event, generator]
         wrapper_args.extend(args)
         self.process = ctx.Process(
             target=self._wrapper, name=name, args=wrapper_args, kwargs=kwargs
@@ -63,8 +69,8 @@ class BGWorker:
 
     def __getstate__(self) -> T.Any:
         state = self.__dict__.copy()
-        if 'qt_helper' in state:
-            del state['qt_helper']
+        if "qt_helper" in state:
+            del state["qt_helper"]
 
         return state
 
@@ -72,7 +78,14 @@ class BGWorker:
         self.qt_helper.start()
         self.process.start()
 
-    def _wrapper(self, pipe: mp.connection.Connection, generator: T.Callable[..., T.Generator], *args: T.Any, **kwargs: T.Any) -> None:
+    def _wrapper(
+        self,
+        pipe: mp.connection.Connection,
+        cancel_event: mp.Event,
+        generator: T.Callable[..., T.Generator],
+        *args: T.Any,
+        **kwargs: T.Any,
+    ) -> None:
         """Wrap generator in bg and pipe results to the main process
 
         All exceptions are caught, forwarded to the foreground, and raised in
@@ -87,6 +100,9 @@ class BGWorker:
         signal.signal(signal.SIGINT, interrupt_handler)
         try:
             for datum in generator(*args, **kwargs):
+                if cancel_event.is_set():
+                    raise EarlyCancellationError("Task was cancelled")  # noqa: TRY301
+
                 pipe.send(datum)
 
             pipe.send(StopIteration())
@@ -94,11 +110,11 @@ class BGWorker:
         except BrokenPipeError:
             pass
 
-        except Exception as e:
-            with contextlib.suppress(BrokenPipeError):
-                pipe.send(e)
+        except EarlyCancellationError as exc:
+            pipe.send(exc)
 
-            print(traceback.format_exc())
+        except Exception as exc:
+            pipe.send(exc)
 
         finally:
             pipe.close()
@@ -108,40 +124,47 @@ class BGWorker:
             return
 
         while self.pipe.poll(0):
-            try:
-                datum = self.pipe.recv()
-            except EOFError:
-                print("Process canceled be user.")
-                self._canceled = True
-                self.qt_helper.canceled.emit()
+            datum = self.pipe.recv()
+
+            if isinstance(datum, StopIteration):
+                self._completed = True
+                self.qt_helper.finished.emit()
                 return
 
-            else:
-                if isinstance(datum, StopIteration):
-                    self._completed = True
-                    self.qt_helper.finished.emit()
-                    return
+            elif isinstance(datum, Exception):
+                if isinstance(datum, EarlyCancellationError):
+                    self.qt_helper.canceled.emit()
 
-                elif isinstance(datum, Exception):
+                else:
+                    # @TODO: log this exception rather than raise it
                     raise datum
 
-                elif isinstance(datum, ProgressUpdate):
-                    self.progress = datum.progress
-                    self.qt_helper.progress_changed.emit(datum.progress)
+            elif isinstance(datum, ProgressUpdate):
+                self.progress = datum.progress
+                self.qt_helper.progress_changed.emit(datum.progress)
 
-    # def cancel(self, timeout:  float = 1) -> None:
-    #     if not (self.completed or self.canceled):
-    #         self._cancel_event.set()
-    #         self.fetch() # flush
+    def cancel(self, timeout: float = 1) -> None:
+        if not (self.completed or self.canceled):
+            self._cancel_event.set()
+            self.fetch()  # flush
 
-    #     if self.process is not None:
-    #         self.process.join(timeout)
+        if self.process is not None:
+            self.process.join(timeout)
+
+    @property
+    def completed(self) -> bool:
+        return self._completed
+
+    @property
+    def canceled(self) -> bool:
+        return self._canceled
 
 
 class JobManager(QObject):
     progress_changed = Signal(float)
     job_started = Signal(BGWorker)
     job_finished = Signal(BGWorker)
+    job_canceled = Signal(BGWorker)
 
     def __init__(self) -> None:
         super().__init__()
@@ -149,7 +172,17 @@ class JobManager(QObject):
         self.bg_workers: list[BGWorker] = []
         self.job_count = 0
 
-    def create_job(self, name: str, generator: T.Callable, *args: T.Any, **kwargs: T.Any) -> BGWorker:
+        app = neon_player.instance()
+        app.aboutToQuit.connect(self.cleanup)
+
+    def cleanup(self) -> None:
+        for worker in self.bg_workers:
+            print(f"JobManager: cleaning up {worker.name}")
+            worker.cancel()
+
+    def create_job(
+        self, name: str, generator: T.Callable, *args: T.Any, **kwargs: T.Any
+    ) -> BGWorker:
         worker = BGWorker(name, generator, *args, **kwargs)
         self.add_job(worker)
         worker.start()
@@ -162,12 +195,19 @@ class JobManager(QObject):
 
         bg_worker.qt_helper.progress_changed.connect(lambda _: self.update_progress())
         bg_worker.qt_helper.finished.connect(lambda: self.on_job_finished(bg_worker))
+        bg_worker.qt_helper.canceled.connect(
+            lambda: self.on_job_finished(bg_worker, True)
+        )
 
-    def on_job_finished(self, bg_worker: BGWorker) -> None:
+    def on_job_finished(self, bg_worker: BGWorker, canceled: bool = False) -> None:
         self.bg_workers.remove(bg_worker)
         self.job_count -= 1
         self.update_progress()
-        self.job_finished.emit(bg_worker)
+
+        if canceled:
+            self.job_canceled.emit(bg_worker)
+        else:
+            self.job_finished.emit(bg_worker)
 
     def update_progress(self) -> None:
         if self.job_count == 0:
@@ -175,8 +215,8 @@ class JobManager(QObject):
             return
 
         completed_job_count = self.job_count - len(self.bg_workers)
-        progress = (completed_job_count + sum(
-            [worker.progress for worker in self.bg_workers]
-        )) / self.job_count
+        progress = (
+            completed_job_count + sum([worker.progress for worker in self.bg_workers])
+        ) / self.job_count
 
         self.progress_changed.emit(progress)
